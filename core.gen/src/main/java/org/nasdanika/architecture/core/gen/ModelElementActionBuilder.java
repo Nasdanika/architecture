cdbd4b6e69598cfa8a96c45ce37249c3362ccce5
Pavel <Pavel.Vlasov@nasdanika.org>
2023-02-28 08:43:50 -0500

    Semantic URI's carry-over

--- core.gen/src/main/java/org/nasdanika/architecture/core/gen/ModelElementActionBuilder.java
+++ core.gen/src/main/java/org/nasdanika/architecture/core/gen/ModelElementActionBuilder.java
@@ -1,13 +1,39 @@
 package org.nasdanika.architecture.core.gen;
 
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.nasdanika.architecture.core.ModelElement;
 import org.nasdanika.common.Context;
+import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.html.emf.NcoreActionBuilder;
+import org.nasdanika.html.model.app.Action;
+import org.nasdanika.ncore.util.NcoreUtil;
 
 public class ModelElementActionBuilder<T extends ModelElement> extends NcoreActionBuilder<T> {
 	
 	public ModelElementActionBuilder(T value, Context context) {
 		super(value, context);		
+	}
+	
+	@Override
+	protected Action buildAction(
+			Action action, 
+			BiConsumer<EObject, Action> registry,
+			Consumer<org.nasdanika.common.Consumer<org.nasdanika.html.emf.EObjectActionResolver.Context>> resolveConsumer,
+			ProgressMonitor progressMonitor) {
+		Action ret = super.buildAction(action, registry, resolveConsumer, progressMonitor);
+		
+		// Carrying over URI's for semantic mapping, discarding relative
+		for (URI uri: NcoreUtil.getUris(getTarget())) {
+			if (!uri.isRelative()) {
+				ret.getUris().add(uri.toString());
+			}
+		}		
+		
+		return ret;
 	}
 	
 }
