4cd55cce17824833e809f49034e201a79567c42a
Pavel <Pavel.Vlasov@nasdanika.org>
2023-08-26 23:50:56 -0400

    Committing old changes

--- cloud/azure/core.doc/src/main/java/org/nasdanika/architecture/cloud/azure/core/doc/CoreDocLoader.java
+++ cloud/azure/core.doc/src/main/java/org/nasdanika/architecture/cloud/azure/core/doc/CoreDocLoader.java
@@ -6,15 +6,16 @@ import org.eclipse.emf.common.util.URI;
 import org.nasdanika.architecture.cloud.azure.core.CorePackage;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.ProgressMonitor;
+import org.nasdanika.html.ecore.gen.processors.DocLoader;
 import org.nasdanika.html.model.app.Action;
 import org.nasdanika.html.model.app.util.AppObjectLoaderSupplier;
-import org.nasdanika.html.model.app.util.DocLoader;
 
 /**
  * Loads documentation actions from classpath resources
  * @author Pavel
  *
  */
+@Deprecated
 public class CoreDocLoader extends DocLoader {
 		
 	public CoreDocLoader(
